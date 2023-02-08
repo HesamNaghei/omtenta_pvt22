3895ec5b1ca4cd31e69d60433e9e1d112c6034b9
HesamNaghei <hesam.naghei@gmail.com>
2023-02-08 15:57:16 +0100

    Omtentan

---
+++ api.py
@@ -0,0 +1,26 @@
+import requests
+from typing import Optional
+
+CATEGORIES = {"fysik": "phy",
+              "kemi": "che",
+              "litteratur": "lit",
+              "ekonomi": "eco",
+              "fred": "pea",
+              "medicin": "med"}
+
+
+def get_info(year: str, category: str = None) -> Optional[dict]:
+    # skickar en förfrågan till api med den information som angavs till funktionen, kontrollerar om category som angavs
+    # finns med i CATEGORIES
+    if category is not None:
+        if category in CATEGORIES:
+            category = CATEGORIES[category]
+            current_search = {"nobelPrizeYear": int(year), "nobelPrizeCategory": category}
+        else:
+            print('Det var ingen giltig kategori du skrev in')
+            return None
+    else:
+        current_search = {"nobelPrizeYear": int(year)}
+        result = requests.get("http://api.nobelprize.org/2.1/nobelPrizes", params=current_search).json()
+
+        return result
--- nobel_prize.py
+++ nobel_prize.py
@@ -1,66 +1,88 @@
-import requests
+# Hesameddin Naghiei
 
-# Tips: använd sidan nedan för att se vilken data vi får tillbaks och hur apiet fungerar
-# vi använder oss enbart av /nobelPrizes
-# Dokumentation, hjälp samt verktyg för att testa apiet fins här: https://app.swaggerhub.com/apis/NobelMedia/NobelMasterData/2.1
+from api import get_info, CATEGORIES
 
 HELP_STRING = """
 Ange ett år och fält
 Exempelvis 1965 fysik
+Skriv in Q för att avsluta
+Skriv in H för att få fram hjälp texten och alla kategorier
 """
-
-cat = {"fysik": "phy",
-       "kemi": "che",
-       "litteratur": "lit",
-       "ekonomi": "eco",
-       "fred": "pea",
-       "medicin": "med"}
-
-
-
-# TODO 10p programmet skall ge en hjälpsam utskrift istället för en krasch om användaren skriver in fel input
-# TODO 15p om användaren inte anger ett område som exempelvis fysik eller kemi så skall den parametern inte skickas med till apiet och vi får då alla priser det året
-
-
-
 
 
 def main():
+    # Frågar användaren efter en string och anropar funktionen som gör det användaren vill
 
     while True:
-        print(HELP_STRING)
-        # TODO 5p Skriv bara ut hjälptexten en gång när programmet startar inte efter varje gång användaren matat in en fråga
-        #      Förbättra hjälputskriften så att användaren vet vilka fält, exempelvis kemi som finns att välja på
+        user_input = input(">").lower()
+        if user_input == 'q':
+            print('Stänger ner programmet')
+            break
+        elif user_input == 'h':
+            print_help_text()
+        else:
+            result = check_user_input(user_input)
+            if result:
+                print_selected_nobel_prizes(result)
 
-        # TODO 5p Gör så att det finns ett sätt att avsluta programmet, om användaren skriver Q så skall programmet stängas av
-        #      Beskriv i hjälptexten hur man avslutar programmet
-        # TODO 5p Gör så att hjälptexten skrivs ut om användaren skriver h eller H
-        aaa = input(">")
-        a, b = aaa.split()
-        c = cat[b]
+
+def print_selected_nobel_prizes(result: dict):
+    # skriver ut all information som användaren har begärt och skickar 2 strings till calculate_prize_share för att få
+    # tillbaka en float
+    for prize in result["nobelPrizes"]:
+        prize_amount = prize["prizeAmount"]
+        prize_amount_today = prize["prizeAmountAdjusted"]
+        print(f"{prize['categoryFullName']['se']} prissumma {prize_amount} SEK")
+        print(f'Dagens värde på priset är {prize_amount_today} SEK')
+        for person in prize["laureates"]:
+            if 'knownName' in person:
+                print(person['knownName']['en'])
+            else:
+                print(person['orgName']['en'])
+            print(person['motivation']['en'])
+            andel = person['portion']
+            print(f'Fick {calculate_prize_share(prize_amount, andel)} SEK utav {prize_amount} SEK')
+            print('-' * 25)
 
 
-        c = {"nobelPrizeYear": int(a),"nobelPrizeCategory":c}
+def calculate_prize_share(prize_amount: str, andel: str) -> float:
+    # Beräknar fram en float till 3 decimaler från två strings som in data och klarar att andel är ett fraktal
+    if '/' in andel:
+        andel = andel.split('/')[-1]
+    prize = int(prize_amount) / int(andel)
+    prize = round(prize, 3)
+    return prize
 
-        res = requests.get("http://api.nobelprize.org/2.1/nobelPrizes", params=c).json()
-        # TODO 5p  Lägg till någon typ av avskiljare mellan pristagare, exempelvis --------------------------
 
-        # TODO 20p Skapa en funktion som ger en summering av ett år. Om användaren exempelvis skriver "summering 1965"
-        #   skall programmet skriva ut den totala prissumman det året samt hur många pristagare det var.
-        #   Du skall alltså summera alla priser det året och räkna antalet pristagare.
-        #   Exempel på hur det kan se ut:
-        #   > summering 1965
-        #   År 1965 fick 9 pristagare dela på totalt 1410000 kronor
-        #   Tips: Tänk på alla priser inte delats ut alla år. Ekonomipriset infördes exempelvis 1968
+def check_user_input(user_input: str) -> dict:
+    # Kontrollerar om användaren har skrivit in en giltigt string om string är giltigt så skickas informationen till
+    # get_info annars så skrivs det ut ett felmeddelande till användaren och hur giltigt inmatning ser ut
+    try:
+        year, category = user_input.split()[:2]
+        if year.isnumeric() and category.isalpha():
+            result = get_info(year, category)
+        else:
+            print('Skriv in bara ett årtal eller årtal mellanslag kategori')
+            result = None
+    except ValueError:
+        if user_input.isnumeric():
+            year = user_input
+            result = get_info(year)
+        else:
+            print('Skriv in bara ett årtal eller årtal mellanslag kategori')
+            result = None
+    return result
 
-        for p in res["nobelPrizes"]:
-            peng = p["prizeAmount"]
-            print(f"{p['categoryFullName']['se']} prissumma {peng} SEK")
 
-            for m in p["laureates"]:
-                print(m['knownName']['en'])
-                print(m['motivation']['en'])
+def print_help_text():
+    # Skriver ut hjälp texten och alla kategorier i terminalen
+    print(HELP_STRING)
+    print('Alla möjliga kategorier:')
+    key_list = list(CATEGORIES.keys())
+    for key in key_list:
+        print(key)
 
 
 if __name__ == '__main__':
+    print_help_text()
     main()
